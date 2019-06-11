42238826970dcac8fa5eedfd0106771b8c25a1d5
lvsal <lvsalas@ucalgary.ca>
2019-06-10 20:56:14 -0600

    Added log in, adding/deleting items, editing items, editing customers

--- src/managerMenu.java
+++ src/managerMenu.java
@@ -18,6 +18,7 @@ public class ManagerMenu {
     private JTabbedPane tabbedPane1;
     private JTable saleTable;
     private JTable rentTable;
+    private JButton buttonAdd;
     private JScrollPane scrollPanel;
     private JTable appointmentTable;
     private JButton signOutButton;
@@ -30,8 +31,8 @@ public class ManagerMenu {
 
     private final static String customerInfo[] = {"Customer ID", "Name", "Phone", "Address", "City", "Province"};
 
-    private final static String saleInfo[] = {"ItemID", "Name", "Price", "Stock", "StoreID"};
-    private final static String rentInfo[] = {"ItemID", "Name", "Price", "Stock", "StoreID"};
+    private final static String saleInfo[] = {"ItemID", "Name", "Price", "Stock"};
+    private final static String rentInfo[] = {"ItemID", "Name", "Price", "Stock"};
     private final static String atvInfo[] = {"VIN", "Model", "Price", "Stock", "Ground"};
     private final static String bicycleInfo[] = {"Serial No.", "Model", "Price", "Stock", "Ground"};
 
@@ -41,6 +42,9 @@ public class ManagerMenu {
     private final static String sqlPassword = "2QL01X7xCG";
 
     private final static String queryCust = "Select * from CUSTOMER";
+
+    private final static String querySaleInfo = "Select * from FOR_SALE";
+    private final static String queryRentInfo = "Select * from FOR_RENT";
 
     private PreparedStatement ps;
 
@@ -57,6 +61,7 @@ public class ManagerMenu {
         frame.setSize(width / 2, height / 2);
 
         con = createConnection();
+        displayItems();
         displayCustomers();
         frame.setContentPane(managerPanel);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -88,6 +93,9 @@ public class ManagerMenu {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
+                con = createConnection();
+                refreshItems();
+                refreshCustomers();
             }
         });
         btnLogout.addMouseListener(new MouseAdapter() {
@@ -104,6 +112,8 @@ public class ManagerMenu {
                 }
             }
         });
+
+
         customerTable.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -114,6 +124,41 @@ public class ManagerMenu {
                     EditCustInfo edit = new EditCustInfo(con, key, menu);
                     e.consume();
                 }
+            }
+        });
+
+        // TODO: Open Item Edit Menu
+        saleTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                if (e.getClickCount() == 2) {
+                    int row = saleTable.rowAtPoint(e.getPoint());
+                    int key = (int) saleTable.getValueAt(row, 0);
+                    EditItem item1 = new EditItem(con, key, menu, "sale");
+                    e.consume();
+                }
+            }
+        });
+
+        // TODO: Open Item Edit Menu
+        rentTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                if (e.getClickCount() == 2) {
+                    int row = rentTable.rowAtPoint(e.getPoint());
+                    int key = (int) rentTable.getValueAt(row, 0);
+                    EditItem item2 = new EditItem(con, key, menu, "rent");
+                    e.consume();
+                }
+            }
+        });
+        buttonAdd.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                EditItem newItem = new EditItem(con, menu);
             }
         });
     }
@@ -173,11 +218,9 @@ public class ManagerMenu {
             ps = con.prepareStatement(queryCust);
             ResultSet rs = ps.executeQuery();
             while(rs.next()) {
-                //System.out.println(rs.getInt(1) + " " +rs.getString(2)+ " "+ rs.getString(3)+" "+ rs.getString(5)+
-                //       " "+ rs.getString(6) + " " +  rs.getString(4));
                 Object toAdd[] = {
                         rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(5),
-                            rs.getString(6), rs.getString(4)
+                        rs.getString(6), rs.getString(4)
                 };
                 model.addRow(toAdd);
             }
@@ -186,7 +229,39 @@ public class ManagerMenu {
             e.printStackTrace(new java.io.PrintStream(System.out));
         }
     }
-    protected void refreshManagerMenu() {
+
+    protected void refreshItems() {
+        model2.setRowCount(0);
+        model3.setRowCount(0);
+        displayItems();
+    }
+
+    private void displayItems() {
+        try {
+            ps = con.prepareStatement(querySaleInfo);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                Object toAdd[] = {
+                        rs.getInt(1), rs.getString(3), rs.getDouble(2), rs.getInt(4)
+                };
+                model2.addRow(toAdd);
+            }
+            ps = con.prepareStatement(queryRentInfo);
+            rs = ps.executeQuery();
+            while(rs.next()) {
+                Object toAdd[] = {
+                        rs.getInt(1), rs.getString(3), rs.getDouble(2), rs.getInt(4)
+                };
+                model3.addRow(toAdd);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace(new java.io.PrintStream(System.out));
+        }
+    }
+
+    protected void refreshCustomers() {
         model.setRowCount(0);
         displayCustomers();
     }
